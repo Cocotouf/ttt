a4f85f254e05edf0c2f2c003f5da4377c4f99b9e
cfenet <christophe.fenet@docapost.fr>
2016-05-29 19:14:21 +0200

    table

--- src/js/players/playersCtrl.js
+++ src/js/players/playersCtrl.js
@@ -5,7 +5,7 @@ angular.module('ttt')
 .controller('PlayersController', PlayersController);
 
 function PlayersController($scope) {
-  $scope.test = 'toto';
+	
 }
 
 PlayersController.$inject = ['$scope'];
