0192dded1623c1f57b3cd36b18564f35e496d6b1
cfenet <christophe.fenet@docapost.fr>
2016-05-29 22:26:52 +0200

    playersSvc + player insertion

--- src/js/players/playersCtrl.js
+++ src/js/players/playersCtrl.js
@@ -4,9 +4,27 @@
 angular.module('ttt')
 .controller('PlayersController', PlayersController);
 
-function PlayersController($scope, Players, $timeout) {
+function PlayersController($scope, Players) {
 	$scope.players = Players.getPlayers;
+	$scope.newPlayer = {
+		lastname: '',
+		firstname: '',
+		points: 500
+	};
+	
+	$scope.addPlayer = function() {
+		Players.addPlayer(angular.copy($scope.newPlayer));
+		$scope.newPlayer = {
+			lastname: '',
+			firstname: '',
+			points: 500
+		};
+		$scope.newPlayerForm.$setUntouched()
+	};
+	
+	$scope.removePlayer = Players.removePlayer;
+	$scope.cancelRemovePlayer = Players.cancelRemovePlayer;
 }
 
-PlayersController.$inject = ['$scope', 'PlayersService', '$timeout'];
+PlayersController.$inject = ['$scope', 'PlayersService'];
 })();
--- src/js/players/playersSvc.js
+++ src/js/players/playersSvc.js
@@ -4,21 +4,34 @@
 angular.module('ttt')
 .service('PlayersService', PlayersService);
 
-function PlayersService() {
+function PlayersService($timeout) {
 	var players = [{lastname: 'Fenet', firstname: 'Christophe', points: 1174}, {lastname: 'Riot', firstname: 'Philippe', points: 612}, {lastname: 'Fenet', firstname: 'Régis', points: 500}];
+	var removeTimeout;
 	
 	return {
+		getPlayers: function() {
+			return players;
+		},
+		getCount: function() {
+			return players.length;
+		},
 		addPlayer: function(player) {
 			players.push(player);
 		},
 		removePlayer: function(player) {
-			players.splice(players.indexOf(player, 1));
+			player.removed = true;
+			removeTimeout = $timeout(function() {
+				players.splice(players.indexOf(player), 1);
+			}, 2000);
 		},
-		getPlayers: function() {
-			return players;
+		cancelRemovePlayer: function(player) {
+			if ($timeout.cancel(removeTimeout)) {
+				delete player.removed;
+			}
+			
 		}
 	}
 }
 
-PlayersService.$inject = [];
+PlayersService.$inject = ['$timeout'];
 })();
